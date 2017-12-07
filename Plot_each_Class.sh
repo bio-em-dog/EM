3008e5b91dffc50346edea92759dbd997f70b1ec
JIA Xudong <SYSUjiaxudong@outlook.com>
2017-12-07 14:57:59 +0800

    1
    1

--- Plot_each_Class.sh
+++ Plot_each_Class.sh
@@ -17,17 +17,36 @@ do
   echo "" >> list_$DATE
 done
 
+#将最后一轮的数据单独列出
 for i in `tail -n 1 list_$DATE`
 do
   echo $i
 done > ClassNumber025
 
+large1=`sort -r -n ClassNumber025 | head -n 1 | tail -n 1`
+large1Pos=`grep -n $large1 ClassNumber025 | cut -d ':' -f 1`
+large2=`sort -r -n ClassNumber025 | head -n 2 | tail -n 1`
+large2Pos=`grep -n $large2 ClassNumber025 | cut -d ':' -f 1`
+large3=`sort -r -n ClassNumber025 | head -n 3 | tail -n 1`
+large3Pos=`grep -n $large3 ClassNumber025 | cut -d ':' -f 1`
+
 #生成gnuplot的命令
 echo -e "set term pdf\nset output \"aaaa_$DATE.pdf\"\nunset key" > gnuplot_script_$DATE
+echo "set label 1 \"$large1\" at $Number_of_iteration,$large1" >> gnuplot_script_$DATE
+echo "set label 2 \"$large2\" at $Number_of_iteration,$large1" >> gnuplot_script_$DATE
+echo "set label 3 \"$large3\" at $Number_of_iteration,$large1" >> gnuplot_script_$DATE
 echo -n "plot \"list_$DATE\" using 1 with lp ls 7" >> gnuplot_script_$DATE
-for i in {2..25}
+for ((i=2;i<=$Number_of_Classes;i++))
 do
-  echo -n ", \"list_$DATE\" u $i w lp ls 7" >> gnuplot_script_$DATE
+  if [ $i = large1Pos ];then
+    echo -n ", \"list_$DATE\" u $i w lp ls 3" >> gnuplot_script_$DATE
+  elif [ $i = large2Pos ];then
+    echo -n ", \"list_$DATE\" u $i w lp ls 4" >> gnuplot_script_$DATE
+  elif [ $i = large3Pos ];then
+    echo -n ", \"list_$DATE\" u $i w lp ls 5" >> gnuplot_script_$DATE
+  else
+    echo -n ", \"list_$DATE\" u $i w lp ls 7" >> gnuplot_script_$DATE
+  fi
 done
 echo -e "\nset output" >> gnuplot_script_$DATE
 
